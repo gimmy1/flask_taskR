0ae595116e7704af891f6014699250059f787d91
gali7221 <gali7221@gmail.com>
2017-08-18 13:38:38 -0400

    Develop RESTful API for all tasks and individual task

--- project/__init__.py
+++ project/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 
 from project.users.views import users_blueprint
 from project.tasks.views import tasks_blueprint
-
+from project.api.views import api_blueprint
 
 # register our blueprints
 app.register_blueprint(users_blueprint)
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import os
 from project import app
 # never have true for production. True also prevents 500 error from popping up.
 # app.run(debug=True)
-app.run()
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
